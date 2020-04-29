a538fa77dd5222e1f589c9460cf9e4f74fcee9c6
Danny <danny.hansen@me.com>
2020-04-29 20:08:34 +0200

    added products with ID routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,3 +2,31 @@ const express = require('express')
 
 router = express.Router()
 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handeling GET request to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'Handeling GET request to /products special ID',
+            id: id
+        });
+    }  else {
+        res.status(200).json({
+            message: 'Handeling GET request to /products ID'
+        });
+    }
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handeling POST request to /products'
+    });
+});
+
+
+module.exports = router;
